ffcdd0ce15eda1725e487d4f52e34da608ebaa14
Benjamin Moss <ben@indusblue.com>
2012-07-27 17:45:50 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoMuchSports::Application.config.secret_token = '137ece19dfce4cd3a18d2ce0110bf078a6590aa04d8a565f3106eaf99dcee350b20979c3eeafc61080e2cbebfb55e2236679c40877b7ae5c6d484a1a1b37fe75'
