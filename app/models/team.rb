3023e13778667c2fa85d8228a6e96d88850d4188
drteeth <drteeth@monkeyhouse.ca>
2012-07-30 23:07:14 -0400

    add nested factories

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ActiveRecord::Base
   attr_accessible :shortname, :division, :league, :name, :nickname
+  has_many :games
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SoMuchSports::Application.routes.draw do
+  get "games/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
