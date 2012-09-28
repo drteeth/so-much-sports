77907444006e666e2895e17c93e51b86275499eb
Benjamin Moss <drteeth@gmail.com>
2012-09-28 11:03:29 -0400

    post merge cleanup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 FactoryGirl.create(:sport_with_periods, name:'MLB')
 FactoryGirl.create(:sport_with_periods, name:'NHL')
 FactoryGirl.create(:sport_with_periods, name:'NFL')
