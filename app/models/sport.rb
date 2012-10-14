50ae8293ca4de5a7da82e2d48aa1d1cbf54486a0
Benjamin Moss <drteeth@gmail.com>
2012-10-14 13:49:43 -0400

    add Sport#names

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -2,4 +2,9 @@ class Sport < ActiveRecord::Base
   attr_accessible :name, :rank
   has_many :periods, :dependent => :destroy
   default_scope :order => :rank
+
+  def self.names
+    all.map(&:name)
+  end
+
 end
