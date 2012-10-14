2e1945114b25b599fdc5d3ae851120df265ae874
Benjamin Moss <drteeth@gmail.com>
2012-10-14 18:55:22 -0400

    use uppercase sport names

--- lib/api.rb
+++ lib/api.rb
@@ -4,18 +4,20 @@ class Api
 
   def order
   	puts "warning: hardcoded order"
-  	'callback({ "sports":"nfl|mlb|cfb|nba|mls" });'
+  	'callback({ "sports":"NFL|MLB|CFB|NBA|MLS" });'
   end
 
   def periods(sports)
     # use &random=1350250416940 for a cache buster if needed
     # nfl|mlb|cfb|nba|golf|mls|nascar
     url = "http://scores.nbcsports.msnbc.com/ticker/data/sports.js.asp?order=#{sports}"
+    puts url
     open(URI.escape(url)).read
   end
 
   def games(sport,period)
     url = "http://scores.nbcsports.msnbc.com/ticker/data/gamesMSNBC.js.asp?sport=#{sport}&period=#{period}"
+    puts url
     open(url).read
   end
 
