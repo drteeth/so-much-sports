21210e41db5425858820212aede6e35181740430
Benjamin Moss <drteeth@gmail.com>
2012-10-14 13:50:37 -0400

    add factory methods to create full sport, period, game, teams

--- spec/factories/periods.rb
+++ spec/factories/periods.rb
@@ -3,5 +3,15 @@ FactoryGirl.define do
     period_id
     label
     is_default false
+
+    factory :period_with_games do
+      ignore do
+        games_count 5
+      end
+
+      after(:create) do |period, evaluator|
+        FactoryGirl.create_list(:game, evaluator.games_count, period:period)
+      end
+    end
   end
 end
--- spec/factories/sports.rb
+++ spec/factories/sports.rb
@@ -2,5 +2,15 @@ FactoryGirl.define do
   factory :sport do
     name
     rank
+
+    factory :sport_with_periods do
+      ignore do
+        periods_count 3
+      end
+
+      after(:create) do |sport, evaluator|
+        FactoryGirl.create_list(:period_with_games, evaluator.periods_count, sport:sport)
+      end
+    end
   end
 end
