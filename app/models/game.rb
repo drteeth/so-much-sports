f792b888903246bb3ef080d33e349c1232bc21b2
Benjamin Moss <drteeth@gmail.com>
2012-10-14 13:47:31 -0400

    games belong to periods

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,21 @@
 class Game < ActiveRecord::Base
-  attr_accessible :channel, :display_status, :game_time, :home_score, :home_team_id, :league, :reason, :status, :type, :url, :visitor_score, :visitor_team_id
+  attr_accessible :channel,
+                  :display_status,
+                  :game_time,
+                  :home_score,
+                  :home_team_id,
+                  :league,
+                  :reason,
+                  :status,
+                  :type,
+                  :url,
+                  :visitor_score,
+                  :visitor_team_id,
+                  :home_team,
+                  :visitor_team,
+                  :period_id
 
+  belongs_to :period
   belongs_to :home_team, :class_name => 'Team'
   belongs_to :visitor_team, :class_name => 'Team'
 
