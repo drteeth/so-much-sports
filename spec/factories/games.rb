4c04d77691ff1eec625e1b33cc63326c5871f8eb
Benjamin Moss <drteeth@gmail.com>
2012-10-14 14:01:44 -0400

    remove factory defaults that are strictly required by model validations

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,17 +1,5 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :game do
-    type ""
-    league "league"
-    status "status"
-    display_status "display_status"
-    url "http://url.org"
-    home_score 2
-    visitor_score 1
-    channel "channel"
-    # reason
-    game_time "2012-07-29 19:58:54"
     association :home_team, factory: :team
     association :visitor_team, factory: :team
   end
